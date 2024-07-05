580cdc901616926b9de5d04e93fc9d0684e6a3c9
JP Zarza <jpzarza@icloud.com>
2024-07-04 21:06:16 -0600

    ai-pronunciation-fixed

--- webApp.py
+++ webApp.py
@@ -34,33 +34,34 @@ def getNext():
     event = {'body':  json.dumps(request.get_json(force=True))}
     return lambdaGetSample.lambda_handler(event, [])
 
-def convert_wav_to_base64_ogg(file):
-  try:
-    wav_file_path = f"./{file.filename}"
-    file.save(wav_file_path)
-    # Load the WAV file
-    audio = AudioSegment.from_wav(wav_file_path)
-    
-    # audio.set_frame_rate(48000) # Set the frame rate to 48000 Hz
-    # Create a BytesIO object to hold the OGG data
-    ogg_buffer = io.BytesIO()
-    
-    # Export the audio data to the OGG format and write it to the BytesIO object
-    audio.export(ogg_buffer, format="ogg")
-    
-    # Get the binary data from the BytesIO object
-    ogg_data = ogg_buffer.getvalue()
-    
-    # Encode the binary data to base64
-    base64_ogg = base64.b64encode(ogg_data).decode('utf-8')
-    
-    # Create the data URI string
-    base64_ogg_str = f"data:audio/ogg;base64,{base64_ogg}"
-    print("Converted to base64 OGG...")
-    return {"output":base64_ogg_str, 'sampleRate': audio.frame_rate}
-  except Exception as e:
-      print(e)
-      return {"output":str(e)}
+def convert_wav_to_base64_3gp(file):
+    try:
+        wav_file_path = f"./{file.filename}"
+        file.save(wav_file_path)
+        
+        # Load the WAV file
+        audio = AudioSegment.from_wav(wav_file_path)
+        
+        # Create a BytesIO object to hold the 3GP data
+        threegp_buffer = io.BytesIO()
+        
+        # Export the audio data to the 3GP format and write it to the BytesIO object
+        audio.export(threegp_buffer, format="3gp")
+        
+        # Get the binary data from the BytesIO object
+        threegp_data = threegp_buffer.getvalue()
+        
+        # Encode the binary data to base64
+        base64_3gp = base64.b64encode(threegp_data).decode('utf-8')
+        
+        # Create the data URI string
+        base64_3gp_str = f"data:audio/3gp;base64,{base64_3gp}"
+        print("Converted to base64 3GP...")
+        return {"output": base64_3gp_str, 'sampleRate': audio.frame_rate}
+    except Exception as e:
+        print(e)
+        return {"output": str(e)}
+
 
 
 def convert_mp3_to_base64_ogg(file):
@@ -104,8 +105,8 @@ def GetAccuracyFromRecordedAudio():
     title = request.form['title']
     language = request.form['language']
     fileDict = ''
-    if file.filename.endswith('.wav'):
-        fileDict = convert_wav_to_base64_ogg(file)
+    if file.filename.endswith('.3gp'):
+        fileDict = convert_wav_to_base64_3gp(file)
     elif file.filename.endswith('.mp3'):
         fileDict = convert_mp3_to_base64_ogg(file)
     event = {'body': json.dumps({"title": title, "base64Audio":fileDict["output"],"language": language, "sampleRate": fileDict['sampleRate']})}
